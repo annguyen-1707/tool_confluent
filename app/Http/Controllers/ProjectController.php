5cdf692996739e230702b28ff4683814ae40ebf9
annguyen <annvhe181169@fpt.edu.vn>
2025-09-29 18:36:12 +0700

    projkect

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -58,12 +58,18 @@ class ProjectController extends Controller
         return response()->json(['message' => 'Deleted']);
     }
 
-    public function addMembersInProject(Request $request, $projectId)
+    public function deleteSoft($id)
     {
-        Log::alert("request", $request->all());
+        $deleted = $this->service->deleteSoft($id);
+        if (!$deleted) return response()->json(['error' => 'Not found project'], 404);
+        return response()->json(['message' => 'Deleted']);
+    }
+
+    public function addMemberInProject($projectId, $userId)
+    {
         try {
-            $userIds = $this->service->addMembersInProject($request->input('userIds'), $projectId);
-            return response()->json($userIds);
+            $userId = $this->service->addMemberInProject($userId, $projectId);
+            return response()->json($userId);
         } catch (\Exception $e) {
             Log::error('Add members failed', [
                 'projectId' => $projectId,
@@ -95,6 +101,29 @@ class ProjectController extends Controller
         $result = $this->service->search($keyword, $perPage);
 
         // format JSON gọn gàng
+        return response()->json([
+            'data' => $result->items(),
+            'meta' => [
+                'current_page' => $result->currentPage(),
+                'per_page'     => $result->perPage(),
+                'total'        => $result->total(),
+                'last_page'    => $result->lastPage(),
+            ]
+        ]);
+    }
+
+
+    public function searchPeopleNotInProject(Request $request, $projectId)
+    {
+        $keyword = $request->input('keyword');              // từ khóa tìm kiếm
+        $perPage = $request->input('per_page', 10);  // mặc định 10 item/trang
+
+        $result = $this->service->searchPeopleNotInProject($keyword, $perPage, $projectId);
+        Log::info("Search request", [
+            'keyword' => $keyword,   // log thêm dữ liệu
+            'perPage' => $perPage,
+            'porjetc' => $projectId
+        ]);        // format JSON gọn gàng
         return response()->json([
             'data' => $result->items(),
             'meta' => [
--- routes/project.php
+++ routes/project.php
@@ -6,9 +6,11 @@ use App\Http\Controllers\ProjectController;
 Route::middleware('jwt.auth')->group(function () {
 
     Route::prefix('projects')->group(function () {
-        Route::put('{project}/add-members', [ProjectController::class, 'addMembersInProject']);
+        Route::put('{project}/add-member/{user}', [ProjectController::class, 'addMemberInProject']);
         Route::put('{project}/remove-member/{user}', [ProjectController::class, 'removeMemberInProject']);
         Route::get('/search', [ProjectController::class, 'search']);
+        Route::delete('soft-delete/{project}/', [ProjectController::class, 'deleteSoft']);
+        Route::get('/search-people-not-in-project/{project}', [ProjectController::class, 'searchPeopleNotInProject']);
     });
     Route::apiResource('projects', ProjectController::class);
 });
